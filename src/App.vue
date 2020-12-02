517a067799ef684226b906b47040f81febfb9101
Glitchfer <arif.0496.rahman@gmail.com>
2020-12-02 20:14:38 +0700

    create project

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'App',
+  components: {},
+  methods: {
+    ...mapActions([])
+  },
+  created() {},
+  mounted() {}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Nunito Sans', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+html,
+body {
+  margin: 0px;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-container fluid id="home">
+    <h1>HOME</h1>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {}
+  },
+  components: {},
+  watch: {},
+  created() {},
+  mounted() {},
+  computed: {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
